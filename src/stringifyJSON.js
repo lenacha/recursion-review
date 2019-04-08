8659d6314b00fb07df2c23128e8c0655e3c5f0e8
lenacha <lenacha32@gmail.com>
2019-04-08 11:57:05 -0700

    just started

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,36 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  return helper(obj)
 };
+
+function helper(obj) {
+  if (typeof obj === number) {
+    return '' +obj;
+  }
+  if (typeof obj === string) {
+    return `"${obj}"`;
+  }
+  if (typeof obj === boolean) {
+    return `"${obj}"`;
+  }
+  if (array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    var result = '[';
+    for (var i = 0; i < obj.length; i++) {
+      result += helper(obj[i]);
+    }
+    result += ']';
+    return result;
+  }
+  if (typeof obj === object) {
+    var result = '{';
+    for (var key in obj) {
+      result += helper(key) + ': ' + helper(obj[key]);
+    }
+    result += '}';
+    return result;
+  }
+}
